05d66559941081f14dda6412d3d4ae41051345ee
Emre Demir <emreedemir@yahoo.com>
2020-11-13 01:05:47 +0300

    1311200102 Frontend Tasarımı Yapıldı

---
+++ eGonullu/Controllers/MyActivityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eGonullu.Controllers
+{
+    public class MyActivityController : Controller
+    {
+        // GET: MyActivity
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eGonullu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eGonullu.MvcApplication" Language="C#" %>
